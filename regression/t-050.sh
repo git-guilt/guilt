3ed501bb7cfed482c872fcabbe58501a0ef2e4fd
Per Cederqvist <ceder@lysator.liu.se>
2026-02-05 20:41:00 +0100

    Add the -n option to "guilt series"
    This produces a patch number for each patch.  The number may change
    when patches are added or removed, folded or split.

--- regression/t-050.sh
+++ regression/t-050.sh
@@ -10,11 +10,14 @@ cmd setup_repo
 cmd guilt series
 
 cmd guilt series -v
+cmd guilt series -n
+cmd guilt series -n -v
 
 guilt series | while read n ; do
 	cmd guilt push
 
 	cmd guilt series -v
+	cmd guilt series -v -n
 done
 
 cmd list_files
