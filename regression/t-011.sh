b24c2dfa497f889feadc0e6564046c2b49d465d4
Frediano Ziglio <frediano.ziglio@cloud.com>
2026-03-27 06:22:23 +0000

    Include "guilt rename" into commands failing checking repository
    t-011 is testing that commands fails if the repository in
    current directory is not properly guilt initialized, add
    rename sub command.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- regression/t-011.sh
+++ regression/t-011.sh
@@ -8,7 +8,7 @@ source "$REG_DIR/scaffold"
 
 cmd setup_git_repo
 
-tests="add applied branch commit delete diff export files fold fork graph header import import-commit new next pop prev push rebase refresh rm series status top unapplied"
+tests="add applied branch commit delete diff export files fold fork graph header import import-commit new next pop prev push rebase refresh rename rm series status top unapplied"
 for t in $tests; do
 	shouldfail guilt $t
 done
