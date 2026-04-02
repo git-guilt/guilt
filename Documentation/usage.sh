36ae60f89368690dc2adc7fc5ebb91e5cfacbfbb
Frediano Ziglio <freddy77@gmail.com>
2026-04-02 14:53:24 +0100

    Improve usage.sh script
    More precise grep expression to extract the line we need.
    Check that grep found the line.
    Use a single sed command to detect errors.
    Use shell syntax to compute command from filename.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- Documentation/usage.sh
+++ Documentation/usage.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+set -e
 name=$(basename $1)
-u=$(grep USAGE $1 |  sed 's/USAGE="//' | sed 's/"$//') 
-echo "'`echo $name | sed -e 's/^guilt-/guilt /'`' $u"  > usage-$name.txt
+u=$(grep '^USAGE="' $1) || {
+	echo "Cannot find usage for $name" >&2
+	exit 1
+}
+u=$(echo "$u" |  sed 's/USAGE="//; s/"$//')
+echo "'guilt ${name#guilt-}' $u"  > usage-$name.txt
